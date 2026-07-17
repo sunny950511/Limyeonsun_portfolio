071df9978cf2d03a79119d34ddfc73b6a4c3f475
sunny950511 <sunny950511@gmail.com>
2026-07-17 13:10:56 +0900

    deploy: 배포 자동 업데이트

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     outDir: "docs",
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v4.js`,
-        chunkFileNames: `assets/[name]-[hash]-v4.js`,
-        assetFileNames: `assets/[name]-[hash]-v4.[ext]`,
+        entryFileNames: `assets/[name]-[hash]-v5.js`,
+        chunkFileNames: `assets/[name]-[hash]-v5.js`,
+        assetFileNames: `assets/[name]-[hash]-v5.[ext]`,
       },
     },
   },
