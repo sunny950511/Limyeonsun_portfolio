1b7b3c9f4cef68d617701fa2254303932dfa70d8
sunny950511 <sunny950511@gmail.com>
2026-07-17 12:10:42 +0900

    chore: 배포 폴더(docs) 삭제 및 vite.config.js 설정 초기 상태로 롤백

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Limyeonsun_portfolio/",
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
-        assetFileNames: `assets/[name]-[hash]-v2.[ext]`
-      }
-    }
-  },
   server: {
     port: 5174,
     strictPort: true,
