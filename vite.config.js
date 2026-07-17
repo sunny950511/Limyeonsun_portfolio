db9659c7c6cd16f725d7120acec9b7cda2c67516
sunny950511 <sunny950511@gmail.com>
2026-07-17 12:37:31 +0900

    chore: set base path and format build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Limyeonsun_portfolio/",
   plugins: [react()],
   build: {
-    outDir: 'docs',
+    outDir: "docs",
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name]-[hash]-v3.js`,
         chunkFileNames: `assets/[name]-[hash]-v3.js`,
-        assetFileNames: `assets/[name]-[hash]-v3.[ext]`
-      }
-    }
+        assetFileNames: `assets/[name]-[hash]-v3.[ext]`,
+      },
+    },
   },
   server: {
     port: 5174,
